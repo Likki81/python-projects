4f145e394e32eea22d231e9121d6c377d940ff4b
Chelakapalli Likhitha <158440717+Likki81@users.noreply.github.com>
2024-07-03 16:04:08 +0530

    Add files via upload

---
+++ word guess.py
@@ -0,0 +1,23 @@
+import random
+words=["hello","hai","likki","ajay"]
+turns=12
+guesses=""
+word=random.choice(words)
+while turns>0:
+    failed=0
+    for char in word:
+        if char in guesses:
+            print(char)
+        else:
+            print("_")
+            failed+=1
+    if failed==0:
+        print("you win")
+        print("word is:",word)
+    guess=input("enter a charcter:")
+    guesses+=guess
+    if guess not in word:
+        turns-=1
+        print("your remaining turns are",turns)
+        if turns==0:
+            print("you loose")
