31e942af53f81b4c1fdb4a8fc89501bc24532fc4
Chelakapalli Likhitha <158440717+Likki81@users.noreply.github.com>
2024-07-18 22:39:27 +0530

    Add files via upload

---
+++ time_countdown.py
@@ -0,0 +1,15 @@
+import time
+def time_countdown(seconds):
+    while True:
+      mins,secs=divmod(seconds,60)
+      timer='{:02d}:{:02d}'.format(mins,secs)
+      print(timer,end='\r')
+      time.sleep(1)
+      seconds-=1
+    print("time out")  
+try:
+    seconds=int(input('enter a secods'))   
+    time_countdown(seconds) 
+except valueError:
+    print("invalid")   
+    
